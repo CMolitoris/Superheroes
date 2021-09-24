ec2f2a7690b7271c41436cf9a32450f19b73c005
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-09-24 16:01:49 -0400

    Delete function added

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -26,13 +26,13 @@ def update(request,hero_id):
         'hero': hero
     }
     if request.method == "POST":
-        name = request.POST.get('name')
-        alter_ego = request.POST.get('alter_ego')
-        primary = request.POST.get('primary_ability')
-        secondary = request.POST.get('secondary_ability')
-        catch_phrase = request.POST.get('catch_phrase')
-        new_hero = Superhero(name=name,alter_ego=alter_ego,primary_ability=primary,secondary_ability=secondary,catch_phrase=catch_phrase)
-        new_hero.save()
+        hero.name = request.POST.get('name')
+        hero.alter_ego = request.POST.get('alter_ego')
+        hero.primary_ability = request.POST.get('primary_ability')
+        hero.secondary_ability = request.POST.get('secondary_ability')
+        hero.catch_phrase = request.POST.get('catch_phrase')
+        #new_hero = Superhero(name=name,alter_ego=alter_ego,primary_ability=primary,secondary_ability=secondary,catch_phrase=catch_phrase)
+        hero.save()
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request,'superheroes/update.html',context)
@@ -49,3 +49,8 @@ def create(request):
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request,'superheroes/create.html')
+
+def delete(request,hero_id):
+    hero = Superhero.objects.get(pk=hero_id)
+    hero.delete()
+    return HttpResponseRedirect(reverse('superheroes:index'))
